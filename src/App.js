361df592f47aeb557a4ac91f07bda959a22b0034
Mohamed Hamada <moha2016226@gmail.com>
2024-03-01 10:44:43 -0800

    change icons btns

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Aside from "./components/navbar-aside";
 import Filter from "./components/filter";
-import {useState} from 'react';
+import { useState } from "react";
 import MidSection from "./components/mid-section";
 
 export default function App() {
-  const[asideOpen,setAside] = useState(false);
- 
+  const [asideOpen, setAside] = useState(false);
+
   return (
     <div className="container">
       <Aside asideOpen={asideOpen} />
-      <div className="aside-btn" onClick={() =>setAside(!asideOpen)}>ASide</div>
+      <div className="aside-btn" onClick={() => setAside(!asideOpen)}>
+        <i className="fas fa-bars" style={{ fontSize: "24px" }}></i>
+      </div>
       <MidSection />
-      
-      <Filter  />
+
+      <Filter />
     </div>
   );
 }
--- src/components/filter.js
+++ src/components/filter.js
@@ -1,15 +1,15 @@
 import FilterApplied from "./filter-applies";
 import Stock from "./filter-stock";
 import ApplyBtn from "./apply-btn";
-import { useState } from "react"
+import { useState } from "react";
 export default function Filter() {
-  const[filterOpen,setFilter] = useState(false);
+  const [filterOpen, setFilter] = useState(false);
 
   return (
     <div className={`filter ${filterOpen ? "open" : ""}`}>
       <h1>Filters</h1>
       <div className="filter-btn" onClick={() => setFilter(!filterOpen)}>
-        Filter
+        <i className="fas fa-filter" style={{ fontSize: "24px" }}></i>
       </div>
       <FilterApplied />
       <Stock />
